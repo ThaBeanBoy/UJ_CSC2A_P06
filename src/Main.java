cdb3964a1cca555ed33b19e4be05b20c6d904abc
ThaBeanBoy <gtchipoyera@gmail.com>
2023-05-01 15:16:14 +0200

    Basic application is running, I set up intellij to run javafx application by setting up the VM Options & adding it as library

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.show();
+    }
+
+    private Parent createContent(){
+        return new StackPane(new Text("Hello World"));
     }
 }
